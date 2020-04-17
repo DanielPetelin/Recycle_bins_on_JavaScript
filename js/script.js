81927f2ad5cc0351e545f7adab4db4452617126d
DanielPetelin <daniilpetelin77@gmail.com>
2020-04-17 18:43:14 +0300

    add new

--- js/script.js
+++ js/script.js
@@ -1,5 +1,38 @@
 let cart = {
-    'trash' : 2,
+    'trush' : 2,
     'waste' : 2,
 };
 
+document.onclick = event => {
+    if (event.target.classList.contains('plus')) {
+        plusFunction(event.target.dataset.id);
+    } else if (event.target.classList.contains('minus')) {
+        minusFunction(event.target.dataset.id);
+    };
+};
+
+const plusFunction = id => {
+    cart[id]++;
+    renderCart();
+};
+
+const minusFunction = id => {
+    if (cart[id] - 1 == 0) {
+        deletFunction(id);
+        return true;
+    };
+    
+    cart[id]--;
+    renderCart();
+};
+
+const deletFunction = id => {
+    delete cart[id];
+    renderCart();
+};
+
+const renderCart = () => {
+    console.log(cart);
+};
+
+renderCart();
